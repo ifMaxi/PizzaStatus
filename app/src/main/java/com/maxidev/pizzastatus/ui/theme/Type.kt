ebfd3c51e8dc35915db28ef8e113476c0d1525fd
Maxi Pelizzoni <maxi96p@gmail.com>
2024-02-06 14:32:57 -0300

    Utilities, colors and fonts added/changed

--- app/src/main/java/com/maxidev/pizzastatus/ui/theme/Type.kt
+++ app/src/main/java/com/maxidev/pizzastatus/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.maxidev.pizzastatus.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.maxidev.pizzastatus.R
+
+val soraFont = FontFamily(
+    Font(R.font.sora, weight = FontWeight.Normal),
+    Font(R.font.sora_medium, weight = FontWeight.Medium)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
